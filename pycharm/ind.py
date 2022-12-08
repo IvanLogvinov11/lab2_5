8979441bdcf1646e0cf618a354db415831679efb
IvanLogvinov11 <logvinov123vanya@mail.ru>
2022-12-08 19:26:08 +0300

    new

---
+++ pycharm/ind.py
@@ -0,0 +1,19 @@
+import random
+
+array = []
+result = None
+size = int(input('Введите размер последовательности: '))
+
+for i in range(1, size + 1):
+    array.append(random.randrange(1, 10000))
+print(array)
+
+for i in range(len(array)):
+    if (array[i]) > (array[i + 1]):
+        result = array[i + 1]
+        break
+
+if result is None:
+    print('Последовательность упорядочена')
+else:
+    print('Первое число, нарушающее упорядоченность: ', result)
